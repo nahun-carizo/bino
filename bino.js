ba0dc29dffed5e7556985e749af3d0fd63cbbfb0
Nahun Carrizo <44736533+nahun-carizo@users.noreply.github.com>
2023-06-30 17:25:01 -0300

    Add files via upload

---
+++ bino.js
@@ -0,0 +1,24 @@
+function ContarMultiplosde2(p){
+    if(p % 2 == 0){
+        return true;
+    }else {
+        return false;
+    }}
+function ContarMultiplosde3(p){
+    if(p % 3 == 0){
+        return true;
+    }else {
+        return false;
+    }}
+function ContarMultiplosde4(p){
+    if(p % 4 == 0){
+        return true;
+    }else {
+        return false;
+    }}
+function ContarMultiplosde5(p){
+    if(p % 5 == 0){
+        return true;
+    }else {
+        return false;
+    }}
